1fe1963e16bdd42d058582e780959e1fc117bd57
Jefferson Aguilar <jeffersonaguilar@outlook.com>
2020-03-05 18:49:58 -0500

    Solved issue 1,2,3

--- website/src/components/RenderAreaChart.jsx
+++ website/src/components/RenderAreaChart.jsx
@@ -6,27 +6,30 @@ import {
   XAxis,
   YAxis,
   Tooltip,
+  ResponsiveContainer,
 } from 'recharts';
 
-const RenderAreaChart = ({data}) => {
+const RenderAreaChart = ({ data }) => {
   return (
-    <AreaChart
-      width={500}
-      height={400}
-      data={data}
-      margin={{
-        top: 10,
-        right: 30,
-        left: 0,
-        bottom: 0,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Area type="monotone" dataKey="value" stroke="#8884d8" fill="#8884d8" />
-      <Tooltip />
-    </AreaChart>
+    <ResponsiveContainer width="100%" height={300}>
+      <AreaChart
+        width={500}
+        height={400}
+        data={data}
+        margin={{
+          top: 10,
+          right: 30,
+          left: 0,
+          bottom: 0,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Area type="monotone" dataKey="value" stroke="#8884d8" fill="#8884d8" />
+        <Tooltip />
+      </AreaChart>
+    </ResponsiveContainer>
   );
 };
 
--- website/src/containers/App.jsx
+++ website/src/containers/App.jsx
@@ -1,43 +1,32 @@
 import React, { useEffect, useState } from 'react';
-import RenderAreaChart from './../components/RenderAreaChart';
-import RenderPieChart from './../components/RenderPieChart';
-import RenderStackedBarChart from './../components/RenderStackedBarChart';
+import RenderAreaChart from '../components/RenderAreaChart';
+import RenderPieChart from '../components/RenderPieChart';
+import RenderStackedBarChart from '../components/RenderStackedBarChart';
+import RenderBubbleChart from '../components/RenderBubbleChart';
+import '../styles/components/App.styl';
 
 const App = () => {
-
-  const [ stats, setStats ] = useState({
-    countConversationsByMonth: [
-      {name: 'Enero', value: 434},
-      {name: 'Febrero', value: 363},
-      {name: 'Marzo', value: 242},
-      {name: 'Abril', value: 842},
-    ],
-    countConversations: 23,
-    groupByRateConversations: [
-      {name: '1', value: 100},
-      {name: '2', value: 200},
-      {name: '3', value: 300},
-      {name: '4', value: 400},
-      {name: '5', value: 500},
-    ],
-    groupByRateConversationsByMonth: [
-      {name: 'Enero', 1: 100, 2: 200, 3: 300, 4: 400, 5: 500},
-      {name: 'Febrero', 1: 234, 2: 454, 3: 453, 4: 978, 5: 500},
-      {name: 'Marzo', 1: 345, 2: 545, 3: 343, 4: 343, 5: 500},
-      {name: 'Abril', 1: 345, 2: 545, 3: 343, 4: 0, 5: 500},
-    ]
+  const [stats, setStats] = useState({
+    countConversationsByMonth: [],
+    countConversations: 0,
+    groupByRateConversations: [],
+    groupByRateConversationsByMonth: [],
   });
 
-  // useEffect(() => {
-  //   fetch('http://localhost:3000/api/conversations/stats')
-  //   .then(data => data.json())
-  //   .then(data => setStats(data));
-  // }, []);
+  useEffect(() => {
+    fetch('http://localhost:3000/api/conversations/stats')
+      .then(data => data.json())
+      .then(data => {
+        setStats(data);
+      });
+  }, []);
 
   return (
     <div className="container">
       <div className="title">
-        <h1>Total de conversations 2019 {stats.countConversations}</h1>
+        <h1>
+          Total de conversations 2019 <b>{stats.countConversations}</b>
+        </h1>
       </div>
       <div className="grid">
         <div className="card">
@@ -50,8 +39,9 @@ const App = () => {
           <RenderStackedBarChart data={stats.groupByRateConversationsByMonth} />
         </div>
         <div className="card">
-
+          <RenderBubbleChart data={stats.groupByRateConversations} />
         </div>
+        <div className="card" />
       </div>
     </div>
   );
