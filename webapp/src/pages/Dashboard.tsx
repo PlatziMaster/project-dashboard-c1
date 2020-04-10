6a0d3c108dffa3173ff7ad634daba384a70b6a8a
Nicolas Molina <nicolas.molina.monroy@gmail.com>
2020-04-09 21:13:06 -0400

    work

--- webapp/src/pages/Dashboard.tsx
+++ webapp/src/pages/Dashboard.tsx
@@ -19,7 +19,7 @@ import RenderPieChart from "./../components/RenderPieChart";
 import RenderStackedBarChart from "./../components/RenderStackedBarChart";
 import "./Dashboard.css";
 
-import { sub, format } from 'date-fns';
+import { sub, format } from "date-fns";
 
 const Dashboard: React.FC = () => {
   const [showModal, setShowModal] = useState(false);
@@ -33,9 +33,11 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     const today = new Date();
-    const endAt = format(today, 'yyyy/MM/dd');
-    const startAt = format(sub(today, {days: 7}), 'yyyy/MM/dd');
-    fetch(`http://localhost:3000/api/conversations/stats?start_date=${startAt}&end_date=${endAt}`)
+    const endAt = format(today, "yyyy/MM/dd");
+    const startAt = format(sub(today, { days: 7 }), "yyyy/MM/dd");
+    fetch(
+      `http://localhost:3000/api/conversations/stats?start_date=${startAt}&end_date=${endAt}`
+    )
       .then(data => data.json())
       .then(data => setStats(data));
   }, []);
@@ -44,7 +46,9 @@ const Dashboard: React.FC = () => {
     setShowModal(false);
     const endAt = option.endAt;
     const startAt = option.startAt;
-    fetch(`http://localhost:3000/api/conversations/stats?start_date=${startAt}&end_date=${endAt}`)
+    fetch(
+      `http://localhost:3000/api/conversations/stats?start_date=${startAt}&end_date=${endAt}`
+    )
       .then(data => data.json())
       .then(data => setStats(data));
   };
@@ -61,29 +65,40 @@ const Dashboard: React.FC = () => {
       </IonHeader>
       <IonContent>
         <div className="grid">
-
           <IonCard className="grid--card">
             <IonCardHeader>
-               <IonCardTitle>Número de conversaciones ({stats.countConversations})</IonCardTitle>
+              <IonCardTitle>
+                Número de conversaciones ({stats.countConversations})
+              </IonCardTitle>
             </IonCardHeader>
             <div className="card--chart">
-              <RenderAreaChart data={stats.countConversationsByTime}></RenderAreaChart>
+              <RenderAreaChart
+                data={stats.countConversationsByTime}
+              ></RenderAreaChart>
             </div>
           </IonCard>
           <IonCard className="grid--card">
             <IonCardHeader>
-               <IonCardTitle>Número de conversaciones por calificación</IonCardTitle>
+              <IonCardTitle>
+                Número de conversaciones por calificación
+              </IonCardTitle>
             </IonCardHeader>
             <div className="card--chart">
-              <RenderPieChart data={stats.groupByRateConversations}></RenderPieChart>
+              <RenderPieChart
+                data={stats.groupByRateConversations}
+              ></RenderPieChart>
             </div>
           </IonCard>
           <IonCard className="grid--card">
             <IonCardHeader>
-               <IonCardTitle>Número de conversaciones por calificación en el tiempo</IonCardTitle>
+              <IonCardTitle>
+                Número de conversaciones por calificación en el tiempo
+              </IonCardTitle>
             </IonCardHeader>
             <div className="card--chart">
-              <RenderStackedBarChart data={stats.groupByRateConversationsByTime}></RenderStackedBarChart>
+              <RenderStackedBarChart
+                data={stats.groupByRateConversationsByTime}
+              ></RenderStackedBarChart>
             </div>
           </IonCard>
         </div>
